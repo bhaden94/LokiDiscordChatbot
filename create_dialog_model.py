5ce41152db0d7a61205ab7a26438d6266e8c16c4
bhaden94 <bhaden94@yahoo.com>
2021-09-04 12:32:53 -0400

    update create dialog file

--- create_dialog_model.py
+++ create_dialog_model.py
@@ -192,7 +192,7 @@ MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
 
 class Args():
     def __init__(self):
-        self.output_dir = 'output-mediuim'
+        self.output_dir = 'output-medium'
         self.model_type = 'gpt2'
         self.model_name_or_path = 'microsoft/DialoGPT-medium'
         self.config_name = 'microsoft/DialoGPT-medium'
@@ -637,30 +637,3 @@ def main(df_trn, df_val):
 
 
 main(trn_df, val_df)
-
-# Load trained model
-# tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-medium')
-# model = AutoModelWithLMHead.from_pretrained('output-mediuim')
-
-# # Let's chat for 4 lines
-# for step in range(4):
-#     # encode the new user input, add the eos_token and return a tensor in Pytorch
-#     new_user_input_ids = tokenizer.encode(input(">> User:") + tokenizer.eos_token, return_tensors='pt')
-#     # print(new_user_input_ids)
-
-#     # append the new user input tokens to the chat history
-#     bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-#     # generated a response while limiting the total chat history to 1000 tokens,
-#     chat_history_ids = model.generate(
-#         bot_input_ids, max_length=200,
-#         pad_token_id=tokenizer.eos_token_id,
-#         no_repeat_ngram_size=3,
-#         do_sample=True,
-#         top_k=100,
-#         top_p=0.7,
-#         temperature=0.8
-#     )
-
-#     # pretty print last ouput tokens from bot
-#     print("LokiBot: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
